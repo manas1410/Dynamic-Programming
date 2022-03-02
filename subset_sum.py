2775f600ce937c1547e0169c8163ddba77edf922
Manas Ranjan Munda <64586228+manas1410@users.noreply.github.com>
2022-03-02 21:24:47 +0530

    Update subset_sum.py

--- subset_sum.py
+++ subset_sum.py
@@ -1,25 +1,26 @@
 #this program will say true is subset sum is present and False if not pResent
 #problem url -  https://www.geeksforgeeks.org/subset-sum-problem-dp-25/
 
-# cook your dish here
 t=[[]]
 arr=[2,3,8,10]
-sum = 5
-t = [[False for j in range(sum+1)] for j in range(len(arr)+1)]
+sums = 6
+t = [[False for j in range(sums+1)] for j in range(len(arr)+1)]
 
 for i in range(len(arr)+1):
-    for j in range(sum+1):
+    for j in range(sums+1):
         if i ==0:
             t[i][j]=False
         if j == 0:
             t[i][j]=True
-#print(t)            
-
-for i in range(len(arr)+1):
-    for j in range(sum+1):
-        if arr[i-1]>j:
-            t[i][j] = t[i-1][j]
-        else:
-            t[i][j] =  t[i-1][j-arr[i-1]] or t[i-1][j]
-#print(t)
-print(t[len(arr)][sum])
+            
+def subsetsum(arr,sums):
+    for i in range(len(arr)+1):
+        for j in range(sums+1):
+            if arr[i-1]>j:
+                t[i][j] = t[i-1][j]
+            else:
+                t[i][j] =  t[i-1][j-arr[i-1]] or t[i-1][j]
+    return t[len(arr)][sums] 
+  
+    
+print(subsetsum(arr,sums))
