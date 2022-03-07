febfb981c04a9ea7acabdec3c1b1142eb363de3c
Manas Ranjan Munda <64586228+manas1410@users.noreply.github.com>
2022-03-07 13:44:00 +0530

    Create Print shortest common Supersequence.py

---
+++ Print shortest common Supersequence.py
@@ -0,0 +1,38 @@
+
+'''
+url - https://www.geeksforgeeks.org/print-shortest-common-supersequence/
+'''
+x=input() 
+y=input() 
+l=0
+t = [[0 for j in range(len(y)+1)] for i in range(len(x)+1)]
+for i in range(1,len(x)+1):
+    for j in range(1,len(y)+1):
+        if x[i-1] == y[j-1]:
+            t[i][j] = t[i-1][j-1]+1
+            
+        else:
+            t[i][j] = max(t[i-1][j],t[i][j-1])
+i=len(x)
+j=len(y)
+res = ''
+while(i>0 and j>0):
+    if x[i-1] == y[j-1]:
+        res+=x[i-1]
+        i-=1 
+        j-=1 
+    else:
+        if t[i][j-1]>t[i-1][j]:
+            res+=y[j-1]
+            j-=1 
+        else:
+            res+=x[i-1]
+            i-=1
+while(i>0):
+    res+=x[i-1]
+    i-=1 
+while(j>0):
+    res+=y[j-1]
+    j-=1
+print(res[::-1])
+    
