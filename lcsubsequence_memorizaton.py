cb4f4ab8fb1325503570c501322dd3c29179be64
Manas Ranjan Munda <64586228+manas1410@users.noreply.github.com>
2022-03-07 09:24:39 +0530

    Update lcsubsequence_memorizaton.py

--- lcsubsequence_memorizaton.py
+++ lcsubsequence_memorizaton.py
@@ -5,9 +5,9 @@ def lcs(x,y,n,m):
     if dp[n][m]!=-1:
         return dp[n][m]
     if x[n-1] == y[m-1]:
-        return t[n][m] = 1+lcs(x,y,n-1,m-1)
+        return dp[n][m] = 1+lcs(x,y,n-1,m-1)
     else:
-        return t[n][m] = max(lcs(x,y,n,m-1),lcs(x,y,n-1,m))
+        return dp[n][m] = max(lcs(x,y,n,m-1),lcs(x,y,n-1,m))
         
     
 x=input()
